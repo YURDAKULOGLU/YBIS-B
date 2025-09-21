34d7151fbc31eac85c7bc8ba4589708ddb03fc6b
YURDAKULOGLU <yurdakul.yurdakuloglu@gmail.com>
2025-09-21 18:53:15 +0300

    Configure mobile bundling and CI pipeline

--- apps/mobile/src/stores/chatStore.ts
+++ apps/mobile/src/stores/chatStore.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid/non-secure';
 import { ChatMessage } from '@ybis/core';
 
 export interface Message extends ChatMessage {
